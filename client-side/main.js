fb48f4239571769bf60aa27938d456a26b2ef9f9
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-03-25 14:01:05 -0400

    Working api

--- client-side/main.js
+++ client-side/main.js
@@ -2,7 +2,7 @@ document.querySelector('button').addEventListener('click', getName)
 
 async function getName(){
     try{
-    const res = await fetch('http://localhost:8000/api/recipes/')
+    const res = await fetch('https://simple-recipe-api-100devs.herokuapp.com/api/recipes')
     const data = await res.json()
     
     console.log(data)
